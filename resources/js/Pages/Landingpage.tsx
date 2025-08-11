59ef3dfbf52175b0096936f015a64f79403e0337
Fadel <fadelmaulana12@gmail.com>
2025-08-11 11:53:22 +0700

    Create new navbar using daisyUI

--- resources/js/Pages/Landingpage.tsx
+++ resources/js/Pages/Landingpage.tsx
@@ -1,8 +1,10 @@
+import Navbar from "@/Components/App/Navbar";
 import { Link } from "@inertiajs/react";
 
 export default function Landingpage (){
     return (
         <div>
+            <Navbar/>
             <h1 className="text-red text-lg">Landingpage</h1>
             <button className="btn btn-primary">Fadel</button>
             <button className="btn btn-secondary">
