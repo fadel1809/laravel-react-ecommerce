5111dfa154da586cac57fa5cb708598cfc923bc3
Fadel <fadelmaulana12@gmail.com>
2025-08-20 20:05:35 +0700

    apply varation

--- app/Filament/Resources/ProductResource/Pages/ProductVariations.php
+++ app/Filament/Resources/ProductResource/Pages/ProductVariations.php
@@ -5,10 +5,6 @@ namespace App\Filament\Resources\ProductResource\Pages;
 use App\Enums\ProductVariationTypeEnum;
 use App\Filament\Resources\ProductResource;
 use Filament\Actions;
-use Filament\Forms\Components\Repeater;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
-use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Pages\EditRecord;
 
@@ -25,6 +21,8 @@ class ProductVariations extends EditRecord
     }
     public function form(Form $form): Form
     {
+        $types = $this->record->variationTypes;
+        $fields = [];
         return $form->schema([
            
         ]);
@@ -32,6 +30,68 @@ class ProductVariations extends EditRecord
     protected function mutateFormDataBeforeFill(array $data): array
     {
         $variations = $this->record->variations->toArray();
+        $data['variations'] = $this->mergeCartesianWithExisting($this->record->variationTypes, $variations);
         return $data;
+    }
+    private function mergeCartesianWithExisting($variationTypes,$existingData):array
+    {
+        $defaultQuantity = $this->record->quantity;
+        $defaultPrice = $this->record->price;
+        $cartesianProduct = $this->cartesianProduct($variationTypes,$defaultQuantity,$defaultPrice);
+        $mergeResult = [];
+        foreach($cartesianProduct as $product){
+            $optionIds = collect($product)
+            ->filter(fn($value,$key) => str_starts_with($key,'variation_type'))
+            ->map(fn($option) => $option['id'])
+            ->values()
+            ->toArray();
+
+            $match = array_filter($existingData,function($existingOption) use ($optionIds){
+                return $existingOption['variation_type_option_ids'] === $optionIds;
+            }) ;
+
+            if(!empty($match)){
+                $existingEntry = reset($match);
+                $product['quantity'] = $existingEntry['quantity'];
+                $product['price'] = $existingEntry['price'];
+            }else{
+                $product['quantity'] = $defaultQuantity;
+                $product['price'] = $defaultPrice;
+            }
+            $mergeResult[] = $product;
+        }
+        return $mergeResult;
+    }
+
+    private function cartesianProduct($variationTypes,$defaultQuantity = null,$defaultPrice=null):array {
+        $result = [[]];
+
+        foreach($variationTypes as $index => $variationType){
+            $temp = [];
+
+            foreach($variationTypes->options as $option){
+                foreach($result as $combination){
+                    $newCombination = $combination + [
+                        'variation_type_'. ($variationType->id) => [
+                            'id' => $option->id,
+                            'name' => $option->name,
+                            'label' => $variationType->name,
+
+                        ]
+                    ];
+                    $temp[] = $newCombination;
+                }
+            }
+            $result[] = $temp;
+        }
+
+        foreach($result as $combination){
+            if(count($combination) === count($variationTypes)){
+                $combination['quantity'] = $defaultQuantity;
+                $combination['price'] = $defaultPrice;
+
+            }
+        }
+        return $result;
     }
 }
